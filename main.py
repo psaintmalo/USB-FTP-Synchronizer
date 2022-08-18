da055fbf56b83e00050c788e13d745253103534e
psaintmalo <pdstmalo@gmail.com>
2022-08-18 11:23:59 +0200

    UsbExplorer update returns the change in media

--- main.py
+++ main.py
@@ -5,38 +5,47 @@ import platform
 
 
 class UsbExplorer:
+    # Current media devices stored as an array of tuples (Media Name, Media Path)
     media_devices = []
 
     def __init__(self):
+        # Initialize the object by getting the username and platform to be able to fetch media correctly
         self.user = os.getlogin()
         self.platform = platform.system()
 
+        # Update media array
         self.update()
 
-    def check_changes(self):
+    # Updates the array of available media devices
+    # returns any additions or deletions to the array, "+" as addition and "-" as deletion
+    def update(self):
+
+        # Store current media to calculate differences
         old_media = self.media_devices
-        self.update()
 
-        if old_media == self.media_devices:
-            return False
-        else:
-            non_intersecting_media = list(set(old_media).symmetric_difference(self.media_devices))
-            diff = []
-            for media in non_intersecting_media:
-                # TODO: Append the media tuple of just the name?
-                if media in old_media:
-                    diff.append((media, "-"))
-                else:
-                    diff.append((media, "+"))
+        # Update media array
+        self.media_devices = self.fetch_media(self.user, self.platform)
+
+        # Return differences
+        return self.check_changes(old_media)
+
+    def check_changes(self, old_media):
+
+        # Get media devices not present in both the new and old array
+        non_intersecting_media = list(set(old_media).symmetric_difference(self.media_devices))
+        diff = []
+        # Check if any of the media that has changed has been removed or added
+        for media in non_intersecting_media:
+            # TODO: Append the media tuple of just the name?
+            if media in old_media:
+                diff.append((media, "-"))
+            else:
+                diff.append((media, "+"))
 
         return diff
 
-    def update(self):
-
-        media = self.fetch_media(self.user, self.platform)
-
-        self.media_devices = media
-
+    # Fetch the available media devices utilising platform dependant techniques
+    # returns a tuple array in the form [(Media Name, Media Path)]
     @staticmethod
     def fetch_media(username, platform):
         if platform == "Linux":
@@ -46,23 +55,29 @@ class UsbExplorer:
             media = UsbExplorer.fetch_windows_media()
 
         else:
-            media = "NULL"
+            media = []
             exit("Error: Platform not supported")
 
         return media
 
+    # Fetch the available media in Linux
+    # returns a tuple array in the form [(Media Name, Media Path)]
     @staticmethod
     def fetch_linux_media(username):
+        # Default dir for media in linux
         media_dir = "/run/media/{}/".format(username)
 
+        # Scan the media directory for any subdirectories
         media = [(i.name, i.path) for i in os.scandir(media_dir) if i.is_dir()]
 
         return media
 
     # TODO: Implement
+    # Fetch the available media in Windows
+    # returns a tuple array in the form [(Media Name, Media Path)]
     @staticmethod
     def fetch_windows_media():
-        media = [("TBI", "TBI")]
+        media = []
         print("Windows media discovery not implemented!")
         return media
 
@@ -73,6 +88,7 @@ def main():
 
     print("USB FTP Sync 0.0\n")
 
+    # Initialize USB Explorer
     usb_explorer = UsbExplorer()
 
 
